8df3bdb41c4e5480be16a291f6a056ccd16eefad
hanguanping <youjiannairuo@163.com>
2022-07-04 20:58:13 +0800

    登陆接口和跨域问题

--- service/service_vod/src/main/java/com/youjian/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/youjian/ggkt/vod/controller/TeacherController.java
@@ -25,6 +25,7 @@ import java.util.List;
  */
 @Api(tags = "讲师管理接口")
 @RestController
+@CrossOrigin
 @RequestMapping("/vod/teacher")
 public class TeacherController {
 
---
+++ service/service_vod/src/main/java/com/youjian/ggkt/vod/controller/UserLoginController.java
@@ -0,0 +1,36 @@
+package com.youjian.ggkt.vod.controller;
+
+import com.youjian.ggkt.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Api("登陆接口")
+@RestController
+@CrossOrigin
+@RequestMapping("/vod/user")
+public class UserLoginController {
+
+    @ApiOperation("login")
+    @PostMapping("login")
+    public Result login() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", "admin-token");
+        return Result.ok(map).code(20000);
+    }
+
+    @ApiOperation("info")
+    @GetMapping("info")
+    public Result info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles","[admin]");
+        map.put("name","admin");
+        map.put("avatar","https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+        return Result.ok(map).code(20000);
+    }
+
+
+}
